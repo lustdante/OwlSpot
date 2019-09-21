0579017da4f74f66bae425e2cd699d0737ee286c
jeffrey990219 <qs4@rice.edu>
2019-09-21 18:59:45 -0500

    compute dist routine done

--- frontend/src/components/HotspotPage/index.js
+++ frontend/src/components/HotspotPage/index.js
@@ -10,11 +10,24 @@ class HotspotPage extends React.Component {
     // this.updateLocation = this.updateLocation.bind(this);
   };
 
-  // this.handleClick = this.handleClick.bind(this);
+  computeDist(lat1, lng1, lat2, lng2) {
+    // Returns dist in km.
+    function deg2rad(deg) {
+      return deg * (Math.PI/180)
+    }
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    const dLon = deg2rad(lng2 - lng1);
+    const a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return R * c;
+  }
 
   updateLocation = () => {
     if ('geolocation' in navigator) {
-      console.log('###');
       navigator.geolocation.getCurrentPosition(
         position => {
           let location = { lat: position.coords.latitude, lng: position.coords.longitude };
