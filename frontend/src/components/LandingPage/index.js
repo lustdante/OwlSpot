01c898f16ba8c857e7d2dbda62a8523a44ec9489
jaehyunghwang <jh62@gmail.com>
2019-09-21 16:56:38 -0500

    simple landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import { withRouter } from 'react-router';
+import Image from './rice.jpg';
 
-function LandingPage() {
-  return <div>This is Landing Page</div>;
+const useStyles = makeStyles(theme => ({
+  button: {
+    display: 'flex',
+    margin: '50px',
+  },
+  body: {
+    height: '100vh',
+    backgroundImage: `url(${Image})`,
+  },
+  text: {
+    textAlign: 'center',
+    fontFamily: 'sans-serif',
+    fontSize: '90px',
+    color: 'white',
+  },
+}));
+
+var logIn = props => {
+  props.history.push('/hotspots/');
+};
+
+function LandingPage(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.body}>
+      <Box>
+        <h1 className={classes.text}>OwlSpot</h1>
+        <Button
+          onClick={() => {
+            logIn(props);
+          }}
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          component="span"
+        >
+          <h2>Start Exploring Rice</h2>
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
-export default LandingPage;
+export default withRouter(LandingPage);
