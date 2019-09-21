543e400ddd98ab164f2041f8e3034c9edd88b53d
Jungwoo Lee <jeong1135@gmail.com>
2019-09-21 14:21:35 -0500

    Show inspection and playground in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,11 @@ require('./utils/loadEnv');
 const { ApolloServer } = require('apollo-server');
 const Schema = require('./schema');
 
-const server = new ApolloServer(Schema);
+const server = new ApolloServer({
+  ...Schema,
+  playground: true,
+  introspection: true,
+});
 
 server.listen({ port: process.env.app__PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
