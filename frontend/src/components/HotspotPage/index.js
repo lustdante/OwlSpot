e7e39922f270896c17cdcebef6296346eb0ac215
Jason <jhwang73>
2019-09-21 16:26:13 -0500

    View hotspot name on hotspot page

--- frontend/src/components/HotspotPage/index.js
+++ frontend/src/components/HotspotPage/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import PaperSheet from './hotspotInfo';
 
-function HotspotPage() {
-  return <div>This is Hotspot Page</div>;
+function HotspotPage(props) {
+  const hotspotName = props.match.params.hotspotId
+
+  return (
+    <div>
+      <PaperSheet
+        hotspotName = {hotspotName}
+      />
+      <h2>The camera will be floating here</h2>
+      <h2>The Gallery will be here</h2>
+    </div>
+  );
 }
 
 export default HotspotPage;
