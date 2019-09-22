dbcd3525788e0c6e485e813e0e514937b170aee1
jaehyunghwang <jh62@gmail.com>
2019-09-21 19:36:46 -0500

    commit body part

--- frontend/src/components/HotspotPage/index.js
+++ frontend/src/components/HotspotPage/index.js
@@ -1,41 +1,8 @@
 import React from 'react';
-import Fab from '@material-ui/core/Fab';
-import { makeStyles } from '@material-ui/core/styles';
-import BackIcon from '@material-ui/icons/ArrowBack';
-
 import BodyComponent from './BodyComponent/bodyComponent.js';
-import UploadButton from './floatingUpload';
-
-const useStyles = makeStyles(theme => ({
-  fabBack: {
-    margin: theme.spacing(1),
-    background: 'gray',
-    position: 'fixed',
-    left: '1px',
-    top: '20px',
-  },
-  fabUpload: {
-    margin: theme.spacing(1),
-    background: 'gray',
-    position: 'fixed',
-    right: '1px',
-    top: '20px',
-  },
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-}));
 
 function HotspotPage(props) {
-  const classes = useStyles();
   const hotspotName = props.match.params.hotspotId;
-
-  // Top component
-  // Butotns & Name
-
-  // Bottom Component
-  // Descript + Pic
-  // Gallery
 
   return <BodyComponent hotspotName={hotspotName}></BodyComponent>;
 }
